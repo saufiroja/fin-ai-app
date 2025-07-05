635ea5e1e5e1704bff9f57a50ab5392df1291d9f
saufiroja <saufiroja08@gmail.com>
2025-07-05 19:13:03 +0800

    fix: update API_BASE_URL to use HTTPS for auth, category, chat, transaction, and user slices

--- lib/redux/categorySlice.ts
+++ lib/redux/categorySlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_BASE_URL = "http://103.183.74.179:8080/api/v1";
+const API_BASE_URL = "https://103.183.74.179:8080/api/v1";
 
 // Types
 export interface Category {
